8cb83373c87f1629913b1d927e8aab93905dbadf
joelkbennett <joelkbennett@gmail.com>
2017-01-14 23:19:36 +0000

    Add index route

--- src/boot/routes.jsx
+++ src/boot/routes.jsx
@@ -2,15 +2,16 @@ import React from 'react';
 import { Router, Route, browserHistory, link, IndexRoute } from 'react-router';
 import App from './App';
 
+import FrontPage from 'app/frontPage/index';
+
 class CreateRoutes extends React.Component {
     render() {
         return (
             <Router history={browserHistory}>
                 <Route path='/' component={App}>
 
-                    <IndexRoute component={AboutPage} />
-                    <Route path="for-parents" component={ParentsPage} />
-                    <Route path="parents" component={ParentsPage} />
+                    <IndexRoute component={FrontPage} />
+                    {/*<Route path="parents" component={ParentsPage} />*/}
 
                 </Route>
             </Router>
